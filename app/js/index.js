988ce3c58ded5aa3da0a6ff51e376f84173059f1
Priyanshu Das <das.priyanshu@gmail.com>
2018-02-23 15:26:08 +0530

    show in folder and monitor functionality

--- app/js/index.js
+++ app/js/index.js
@@ -6,9 +6,18 @@
 // downloadItem2.innerHTML = downloadItem.innerHTML;
 //
 // document.body.appendChild(downloadItem2);
+
+let fs = require('fs');
+let path = require('path');
+let {shell} = require('electron');
+
 var ipcRenderer = require('electron').ipcRenderer;
+var remote = require('electron').remote;
 
 var downloadMap = new Map();
+
+var downloadDir = remote.getGlobal('sharedObj').downloadDir;
+
 
 function CreateDOMDownloadItem(item) {
     this.downloadItem = document.createElement("div");
@@ -17,23 +26,32 @@ function CreateDOMDownloadItem(item) {
     this.downloadURL = document.createElement("a");
     this.downloadDetails = document.createElement("div");
     this.downloadButton = document.createElement('button');
+    this.downloadFolder = document.createElement('button');
     this.downloadButtonImg = document.createElement('img');
+    this.downloadFolderImg = document.createElement('img');
     this.downloadBar = document.createElement('div');
     this.progressBar = document.createElement('div');
+    this.channel = item.channel;
 
+    this.downloadBar.appendChild(this.downloadFolder);
     this.downloadBar.appendChild(this.progressBar);
-
     this.downloadButton.appendChild(this.downloadButtonImg);
+    this.downloadFolder.appendChild(this.downloadFolderImg);
     this.downloadItem.className += ' download-item';
     this.downloadImage.className += ' download-item__image';
     this.downloadTitle.className += ' download-item__title';
     this.downloadURL.className += ' download-item__url';
     this.downloadDetails.className += ' download-item__details';
     this.downloadButton.className += ' download-item__button';
+    this.downloadFolder.className += ' download-item__folder';
     this.downloadButtonImg.className += ' button__skin';
     this.downloadButtonImg.src = 'img/download.svg';
+    this.downloadFolderImg.className += ' button__skin';
+    this.downloadFolderImg.src = 'img/folder.svg';
     this.downloadBar.className += ' download-item__downloadBar';
     this.progressBar.className += ' progressBar';
+
+    this.downloadFolder.style.display = 'none';
 
     this.downloadItem.appendChild(this.downloadImage);
     this.downloadItem.appendChild(this.downloadTitle);
@@ -41,6 +59,7 @@ function CreateDOMDownloadItem(item) {
     this.downloadItem.appendChild(this.downloadDetails);
     this.downloadItem.appendChild(this.downloadButton);
     this.downloadItem.appendChild(this.downloadBar);
+    this.downloadItem.appendChild(this.downloadFolder);
 
     this.downloadTitle.innerText = item.name;
     this.downloadImage.src = item.image;
@@ -49,18 +68,36 @@ function CreateDOMDownloadItem(item) {
 
     this.downloadURL.href = item.downloadLink;
     this.downloadURL.innerText = 'Download File';
+
+    // this.downloadExists = false;
+    this.verify = function() {
+        var filePath = path.join(downloadDir, path.basename(this.downloadURL.href));
+        if (fs.existsSync(filePath)) {
+            this.downloadFolder.style.display = 'inline-block';
+            this.progressBar.style.width = '100%';
+        } else {
+            this.downloadFolder.style.display = 'none';
+        }
+    };
+
+    this.showInFolder = function() {
+        var filePath = path.join(downloadDir, path.basename(this.downloadURL.href));
+        shell.showItemInFolder(filePath);
+    }
+    this.verify();
 }
 
 
-function downloadItem(filename = 'Filename', link = 'http://www.pdf995.com/samples/pdf.pdf#pdffile', image = 'img/downloadPlaceholder.svg') {
+function DownloadItem(filename = 'Filename', link = 'http://www.pdf995.com/samples/pdf.pdf', image = 'img/downloadPlaceholder.svg', channel = '#pdffile') {
     this.name = filename;
     this.image = image;
     this.downloadLink = link;
+    this.channel = channel;
 }
 
 function populateDownloads() {
-    var item1 = new downloadItem('PDF');
-    var item2 = new downloadItem('Txt', 'http://25.io/toau/audio/sample.txt#textfile', 'img/TXT.png');
+    var item1 = new DownloadItem('PDF');
+    var item2 = new DownloadItem('Txt', 'http://25.io/toau/audio/sample.txt', 'img/TXT.png', '#txtfile');
 
     var downloadList = [item1, item2];
 
@@ -69,9 +106,12 @@ function populateDownloads() {
         (function() {
             var newDownloadItem = new CreateDOMDownloadItem(downloadList[i]);
             var downloadButton = newDownloadItem.downloadButton;
-            downloadMap.set(newDownloadItem.downloadURL.href, newDownloadItem);
+            downloadMap.set(newDownloadItem.channel, newDownloadItem);
             downloadButton.addEventListener('click', function () {
                 return onClickDownload(newDownloadItem);
+            });
+            newDownloadItem.downloadFolder.addEventListener('click', function() {
+                newDownloadItem.showInFolder();
             });
             document.body.appendChild(newDownloadItem.downloadItem);
         })();
@@ -88,20 +128,27 @@ function updateDownloadPercentage(item, percentage)
 }
 
 function onClickDownload(item) {
+
+    item.verify();
     updateDownloadPercentage(item.downloadItem, 0);
     console.log('sending Item Download Request : ', item);
-    ipcRenderer.send('download-request', item);
-    downloadUrl(item.downloadURL.href);
+    ipcRenderer.send('download-request', item.downloadURL.href, item.channel);
+    // downloadUrl(item.downloadURL.href);
     // updateDownloadPercentage(item.downloadItem, 100);
 }
 
 
 
-    ipcRenderer.on('download-monitor', function(event, id, done, total) {
-    var percentage = done*100/total;
-    console.log('Received Update! : ', id, ' ', done, '/', total);
-    console.log(downloadMap);
-    var item = downloadMap.get(id.toString());
-    console.log('item : ', item);
-    updateDownloadPercentage(downloadMap.get(id).downloadItem, percentage);
+    ipcRenderer.on('download-monitor', function(event, channel, percentage) {
+    // var percentage = done*100/total;
+    // console.log('Received Update! : ', id, ' ', done, '/', total);
+    // console.log(downloadMap);
+    var item = downloadMap.get(channel.toString());
+    // console.log('item : ', item);
+    updateDownloadPercentage(downloadMap.get(channel).downloadItem, percentage);
+    if(percentage == 100)
+    {
+        // item.finished = true;
+        item.verify();
+    }
 });
--- main.js
+++ main.js
@@ -7,12 +7,18 @@ let app = electron.app;
 let BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
-let axios = require('axios');
 
 var appWidth = 400;
 
 var appHeight = 725;
 var ipcMain = require('electron').ipcMain;
+let DownloadManager = require('electron-download-manager');
+
+var downloadDirectory = app.getPath("downloads") + "\\electronDM";
+global.sharedObj = {downloadDir : downloadDirectory};
+
+
+DownloadManager.register({downloadFolder: downloadDirectory});
 
 app.on('ready', function () {
     mainWindow = new BrowserWindow({
@@ -25,36 +31,50 @@ app.on('ready', function () {
     mainWindow.loadURL(`file://${__dirname}/app/index.html`);
 
 
-    mainWindow.webContents.session.on('will-download', (event, item, webContents) => {
-        console.log(item);
-        console.log('webContents : ', webContents);
-        item.setSavePath(`/EDM/${item.getFilename()}`);
-        // var replyChannel = item.getURL().substr(item.getURL().indexOf("#")+1, item.getURL().length);
-        console.log('replyChannel is : ', item.getURL());
-        item.on('updated', (event, state) => {
-            if (state === 'interrupted') {
-                console.log('Download is interrupted but can be resumed');
-            } else if (state === 'progressing') {
-                if(item.isPaused()) {
-                    console.log('Download is paused');
-                } else {
-                    mainWindow.webContents.send('download-monitor', item.getURL().toString(), item.getReceivedBytes(), item.getTotalBytes());
-                    console.log(`Received bytes: ${item.getReceivedBytes()}`);
-                }
-            }
-        })
-
-        item.once('done', (event, state) => {
-            if(state === 'completed') {
-                console.log('Download successful');
-            } else {
-                console.log(`Download Failed: ${state}`);
-            }
-        })
-    });
+    // mainWindow.webContents.session.on('will-download', (event, item, webContents) => {
+    //     console.log(item);
+    //     console.log('webContents : ', webContents);
+    //     item.setSavePath(`/EDM/${item.getFilename()}`);
+    //     // var replyChannel = item.getURL().substr(item.getURL().indexOf("#")+1, item.getURL().length);
+    //     console.log('replyChannel is : ', item.getURL());
+    //     item.on('updated', (event, state) => {
+    //         if (state === 'interrupted') {
+    //             console.log('Download is interrupted but can be resumed');
+    //         } else if (state === 'progressing') {
+    //             if(item.isPaused()) {
+    //                 console.log('Download is paused');
+    //             } else {
+    //                 mainWindow.webContents.send('download-monitor', item.getURL().toString(), item.getReceivedBytes(), item.getTotalBytes());
+    //                 console.log(`Received bytes: ${item.getReceivedBytes()}`);
+    //             }
+    //         }
+    //     })
+    //
+    //     item.once('done', (event, state) => {
+    //         if(state === 'completed') {
+    //             console.log('Download successful');
+    //         } else {
+    //             console.log(`Download Failed: ${state}`);
+    //         }
+    //     })
+    // });
 });
 
 
-ipcMain.on('download-request', function(event, arg) {
-    console.log('Received Download Request : ',arg);
-})
+ipcMain.on('download-request', function(event, url, channel) {
+    // console.log('Received Download Request : ',arg);
+    console.log('Downloading to : ',downloadDirectory);
+    DownloadManager.download({
+        url: url,
+        onProgress: function(progress) {
+            mainWindow.webContents.send('download-monitor', channel, progress);
+        }
+    }, function(error, url){
+        if(error){
+            console.log("ERROR: " + url);
+            return;
+        }
+
+        console.log("DONE: " + url);
+    });
+});
